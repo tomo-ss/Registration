9257e5b432a5a5523130ad9db36b0d16b13b7c16
tomo-ss <mssp.0321@outlook.jp>
2025-08-01 20:54:10 +0900

    regist_complete.php実行完了

---
+++ regist_complete.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+  <meta charset="UTF-8">
+  <title>登録完了</title>
+  <link rel="stylesheet" href="css/regist_complete.css">
+</head>
+<body>
+
+  <h1>登録完了しました</h1>
+  <a href="index.html" class="back-button">TOPページへ戻る</a>
+
+</body>
+</html>
