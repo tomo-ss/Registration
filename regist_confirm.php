069953a05f9aa69f1e966028197858095f9d1769
tomo-ss <mssp.0321@outlook.jp>
2025-08-03 23:11:18 +0900

    ２つのボタンが同じhiddenデータを共有しているため重複していた箇所を１つに修正

--- regist_confirm.php
+++ regist_confirm.php
@@ -14,18 +14,16 @@ $authority        = $_POST['authority'];
 
 $hidden_password = str_repeat("●", strlen($password));
 ?>
-
 <!DOCTYPE html>
 <html lang="ja">
 <head>
   <meta charset="UTF-8">
   <title>アカウント確認画面</title>
-  <link rel="stylesheet" type="text/css" href="confirm.css"> 
+  <link rel="stylesheet" type="text/css" href="confirm.css">
 </head>
 
 <body>
-
- <header>
+  <header>
     <ul>
       <li><a href="index.html">トップ</a></li>
       <li>プロフィール</li>
@@ -38,61 +36,47 @@ $hidden_password = str_repeat("●", strlen($password));
   </header>
 
   <main>
-  <h1>アカウント登録内容確認</h1>
-  <table>
-    <tr><th>名前（姓）</th><td><?= htmlspecialchars($last_name) ?></td></tr>
-    <tr><th>名前（名）</th><td><?= htmlspecialchars($first_name) ?></td></tr>
-    <tr><th>カナ（姓）</th><td><?= htmlspecialchars($last_name_kana) ?></td></tr>
-    <tr><th>カナ（名）</th><td><?= htmlspecialchars($first_name_kana) ?></td></tr>
-    <tr><th>メールアドレス</th><td><?= htmlspecialchars($email) ?></td></tr>
-    <tr><th>パスワード</th><td><?= $hidden_password ?></td></tr>
-    <tr><th>性別</th><td><?= htmlspecialchars($gender) ?></td></tr>
-    <tr><th>郵便番号</th><td><?= htmlspecialchars($postcode) ?></td></tr>
-    <tr><th>住所（都道府県）</th><td><?= htmlspecialchars($prefecture) ?></td></tr>
-    <tr><th>住所（市区町村）</th><td><?= htmlspecialchars($city) ?></td></tr>
-    <tr><th>住所（番地）</th><td><?= htmlspecialchars($address) ?></td></tr>
-    <tr><th>アカウント権限</th><td><?= htmlspecialchars($authority) ?></td></tr>
-  </table>
+    <h1>アカウント登録内容確認</h1>
+    <table>
+      <tr><th>名前（姓）</th><td><?= htmlspecialchars($last_name) ?></td></tr>
+      <tr><th>名前（名）</th><td><?= htmlspecialchars($first_name) ?></td></tr>
+      <tr><th>カナ（姓）</th><td><?= htmlspecialchars($last_name_kana) ?></td></tr>
+      <tr><th>カナ（名）</th><td><?= htmlspecialchars($first_name_kana) ?></td></tr>
+      <tr><th>メールアドレス</th><td><?= htmlspecialchars($email) ?></td></tr>
+      <tr><th>パスワード</th><td><?= $hidden_password ?></td></tr>
+      <tr><th>性別</th><td><?= htmlspecialchars($gender) ?></td></tr>
+      <tr><th>郵便番号</th><td><?= htmlspecialchars($postcode) ?></td></tr>
+      <tr><th>住所（都道府県）</th><td><?= htmlspecialchars($prefecture) ?></td></tr>
+      <tr><th>住所（市区町村）</th><td><?= htmlspecialchars($city) ?></td></tr>
+      <tr><th>住所（番地）</th><td><?= htmlspecialchars($address) ?></td></tr>
+      <tr><th>アカウント権限</th><td><?= htmlspecialchars($authority) ?></td></tr>
+    </table>
 
+  
+    <form method="POST">
+      <input type="hidden" name="last_name" value="<?= htmlspecialchars($last_name) ?>">
+      <input type="hidden" name="first_name" value="<?= htmlspecialchars($first_name) ?>">
+      <input type="hidden" name="last_name_kana" value="<?= htmlspecialchars($last_name_kana) ?>">
+      <input type="hidden" name="first_name_kana" value="<?= htmlspecialchars($first_name_kana) ?>">
+      <input type="hidden" name="email" value="<?= htmlspecialchars($email) ?>">
+      <input type="hidden" name="password" value="<?= htmlspecialchars($password) ?>">
+      <input type="hidden" name="gender" value="<?= htmlspecialchars($gender) ?>">
+      <input type="hidden" name="postcode" value="<?= htmlspecialchars($postcode) ?>">
+      <input type="hidden" name="prefecture" value="<?= htmlspecialchars($prefecture) ?>">
+      <input type="hidden" name="city" value="<?= htmlspecialchars($city) ?>">
+      <input type="hidden" name="address" value="<?= htmlspecialchars($address) ?>">
+      <input type="hidden" name="authority" value="<?= htmlspecialchars($authority) ?>">
 
-  <form action="regist_insert.php" method="POST">
-    <input type="hidden" name="last_name" value="<?= htmlspecialchars($last_name) ?>">
-    <input type="hidden" name="first_name" value="<?= htmlspecialchars($first_name) ?>">
-    <input type="hidden" name="last_name_kana" value="<?= htmlspecialchars($last_name_kana) ?>">
-    <input type="hidden" name="first_name_kana" value="<?= htmlspecialchars($first_name_kana) ?>">
-    <input type="hidden" name="email" value="<?= htmlspecialchars($email) ?>">
-    <input type="hidden" name="password" value="<?= htmlspecialchars($password) ?>">
-    <input type="hidden" name="gender" value="<?= htmlspecialchars($gender) ?>">
-    <input type="hidden" name="postcode" value="<?= htmlspecialchars($postcode) ?>">
-    <input type="hidden" name="prefecture" value="<?= htmlspecialchars($prefecture) ?>">
-    <input type="hidden" name="city" value="<?= htmlspecialchars($city) ?>">
-    <input type="hidden" name="address" value="<?= htmlspecialchars($address) ?>">
-    <input type="hidden" name="authority" value="<?= htmlspecialchars($authority) ?>">
+      
+      <div class="button-wrapper">
+        <button type="submit" formaction="regist_insert.php">登録する</button>
+        <button type="submit" formaction="regist.php">前に戻る</button>
+      </div>
+    </form>
+  </main>
 
-    <button type="submit">登録する</button>
-  </form>
-
-  <form action="regist.php" method="POST">
-    <input type="hidden" name="last_name" value="<?= htmlspecialchars($last_name) ?>">
-    <input type="hidden" name="first_name" value="<?= htmlspecialchars($first_name) ?>">
-    <input type="hidden" name="last_name_kana" value="<?= htmlspecialchars($last_name_kana) ?>">
-    <input type="hidden" name="first_name_kana" value="<?= htmlspecialchars($first_name_kana) ?>">
-    <input type="hidden" name="email" value="<?= htmlspecialchars($email) ?>">
-    <input type="hidden" name="password" value="<?= htmlspecialchars($password) ?>">
-    <input type="hidden" name="gender" value="<?= htmlspecialchars($gender) ?>">
-    <input type="hidden" name="postcode" value="<?= htmlspecialchars($postcode) ?>">
-    <input type="hidden" name="prefecture" value="<?= htmlspecialchars($prefecture) ?>">
-    <input type="hidden" name="city" value="<?= htmlspecialchars($city) ?>">
-    <input type="hidden" name="address" value="<?= htmlspecialchars($address) ?>">
-    <input type="hidden" name="authority" value="<?= htmlspecialchars($authority) ?>">
-
-    <button type="submit">前に戻る</button>
-  </form>
-</main>
-
-<footer>
-  Copyright D.I.Works D.I.blog is the one which provides A to Z about programming
-</footer>
-
+  <footer>
+    Copyright D.I.Works D.I.blog is the one which provides A to Z about programming
+  </footer>
 </body>
 </html>
