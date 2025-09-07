ddcfe5377473d8948ebc2e293d576f47abbf1c27
tomo-ss <mssp.0321@outlook.jp>
2025-09-07 17:35:08 +0900

    アカウント更新完了画面作成完了

---
+++ update_complete.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+  <meta charset="UTF-8">
+  <title>更新完了</title>
+  <link rel="stylesheet" type="text/css" href="update_complete.css">
+</head>
+<body>
+  <header>
+    <ul>
+      <li><a href="index.html">トップ</a></li>
+      <li>プロフィール</li>
+      <li>D.I.Blogについて</li>
+      <li>登録フォーム</li>
+      <li>問い合わせ</li>
+      <li><a href="regist.php">アカウント登録</a></li>
+      <li><a href="list.php">アカウント一覧</a></li>
+      <li>その他</li>
+    </ul>
+  </header>
+
+  <main>
+   <div class="container">
+    <h1>アカウント更新完了画面</h1>
+    <p>更新完了しました。</p>
+     <form action="index.html" method="get">
+    <button type="submit">TOPページへ戻る</button>
+</form>
+  </div>
+  </main>
+
+  <footer>
+    Copyright D.I.Works D.I.blog is the one which provides A to Z about programming
+  </footer>
+</body>
+</html>
